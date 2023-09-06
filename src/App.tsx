2faf198a554c6ddb73162afc3bdd12bba6a87cb9
Joel Estumano <joelestumano@gmail.com>
2023-09-06 18:51:33 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import { Icon } from "./components/icon.component";
+import { PedidoComponent } from "./components/pedido.component";
 
-function App() {
+function OffCanvasExample({ ...props }) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const toggleShow = () => setShow((s) => !s);
+
+  const [pedidos, setPedidos] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/v1/pedidos/paginate?status=pendente', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }).then(resp => resp.json())
+      .then((resp) => {
+        setPedidos(resp.documentos)
+      }).catch((err) => {
+        console.error(err);
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button variant="primary" onClick={toggleShow} className="me-2 rounded-0">
+        <Icon iconName="List" color="white" size={32} className="align-top" />
+      </Button>
+      <Offcanvas show={show} onHide={handleClose} {...props}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          Some text as placeholder. In real life you can have the elements you
+          have chosen. Like, text, images, lists, etc.
+        </Offcanvas.Body>
+      </Offcanvas>
+
+      <div className="container">
+        <ul className="list-group">
+          {pedidos.length > 0 && pedidos.map((pedido: any, index) => <li key={index} className="list-group-item border-0">
+            <PedidoComponent pedido={pedido} />
+          </li>)}
+        </ul>
+      </div>
+
     </div>
   );
+}
+
+function App() {
+  const props = {
+    scroll: true,
+    backdrop: true,
+  };
+  return <OffCanvasExample {...props} />;
 }
 
 export default App;
---
+++ src/components/pedido.component.tsx
@@ -0,0 +1,23 @@
+import { Icon } from "./icon.component";
+
+export const PedidoComponent = ({ ...props }) => {
+  let test = new Date(props.pedido?.horaDespacho);
+  return (
+    <>
+      <div className="card border-primary">
+        <div className="card-body">
+          <h5 className="card-title">{props.pedido?.cliente.nome} <Icon iconName="Clock" color="white" size={24} className="align-middle" /> {test.toLocaleTimeString()}</h5>
+          <p className="card-text">{props.pedido.status}</p>
+          <ul className="list-group">
+            {props.pedido.items.length > 0 &&
+              props.pedido.items.map((item: any, index: number) => <li key={index} className="list-group-item border-0">
+                <img src={item.bannerUrl} className="" height={64} width={64} alt="item" />
+                <p>R$: {item.valor} de {item.descricao}</p>
+              </li>
+              )}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};

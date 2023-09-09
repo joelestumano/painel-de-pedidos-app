a7d6c68294370c06fb58ff7b899b74ed63d89200
Joel Estumano <joelestumano@gmail.com>
2023-09-09 14:55:41 -0300

    novo-layout

---
+++ src/components/NadaPorAqui/NadaPorAqui.tsx
@@ -0,0 +1,15 @@
+import { Col, Container, Row } from "react-bootstrap";
+import "./NadaPorAqui.scss";
+export function NadaPorAqui({ ...props }) {
+    return (
+        <Container fluid={true} className="bg-img">
+            <Row className="w-100">
+                <Col className="col-12 col-md-6 text-center">
+                    <label className="fw-semibold bg-white bg-opacity-75 p-2 rounded fs-1">
+                        {props.titulo}
+                    </label>
+                </Col>
+            </Row>
+        </Container>
+    )
+}

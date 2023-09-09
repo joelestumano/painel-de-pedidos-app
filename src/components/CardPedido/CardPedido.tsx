1c8322d4a3f2494a228b514299c447b002d5dade
Joel Estumano <joelestumano@gmail.com>
2023-09-09 14:03:35 -0300

    novo-layout

--- src/components/CardPedido/CardPedido.tsx
+++ src/components/CardPedido/CardPedido.tsx
@@ -3,6 +3,7 @@ import { Pedido } from "../../types/Pedido.type";
 import { ImageGallery } from "../ImageGallery/ImageGallery";
 import { NumeroPedido } from "../NumeroPedido/NumeroPedido";
 import { Icon } from "../Icon";
+import { Detalhes } from "../Detalhes/Detalhes";
 
 const Section = ({ ...props }) => {
     return (
@@ -21,36 +22,56 @@ export const CardPedido = ({ ...props }) => {
 
     return (
         <Card className="h-100 border-0 p-2 bg-transparent">
-            <Row className={`h-100  ${props.isPrincipal ? "bg-white bg-opacity-50 shadow rounded-start" : "bg-primary bg-opacity-10 rounded"}`}>
+            <Row
+                className={`h-100 rounded ${props.isPrincipal
+                        ? "bg-white bg-opacity-50 shadow"
+                        : "bg-primary bg-opacity-10"
+                    }`}
+            >
                 <Col className="col-12 col-md-6 p-3">
                     <ImageGallery images={getImgs(props.pedido)} />
                 </Col>
                 <Col className="col-12 col-md-6 p-3 ps-md-0">
-                    {props.isPrincipal ? (
-                        <div className="d-flex align-items-center mb-3">
-                            <Section cliente={props.pedido.cliente.nome} />
-                        </div>
-                    ) : null}
-                    <NumeroPedido value={props.pedido.codigo} />
-                    <h6>
-                        <Icon iconName="Clock" size={16} className="align-middle me-1" />
-                        {despacho.toLocaleDateString()} às {despacho.toLocaleTimeString()}
-                    </h6>
-                    <ul className="list-group">
-                        {props.pedido.items.map((item: any, index: number) => (
-                            <li
-                                className="list-group-item border-0 bg-transparent p-0 my-1"
-                                key={index}
-                            >
-                                <span className="card-text p-1 px-2 rounded-2 bg-primary bg-opacity-25">
-                                    <small className="">
-                                        {item.descricao} R${" "}
-                                        {item.valor.toFixed(2).replace(".", ",")}
-                                    </small>
-                                </span>
-                            </li>
-                        ))}
-                    </ul>
+                    <Row>
+                        <Col>
+                            {props.isPrincipal ? (
+                                <div className="d-flex align-items-center mb-3">
+                                    <Section cliente={props.pedido.cliente.nome} />
+                                </div>
+                            ) : null}
+                            <NumeroPedido value={props.pedido.codigo} />
+                            <h6>
+                                <Icon
+                                    iconName="Clock"
+                                    size={16}
+                                    className="align-middle me-1"
+                                />
+                                {despacho.toLocaleDateString()} às{" "}
+                                {despacho.toLocaleTimeString()}
+                            </h6>
+                            <ul className="list-group">
+                                {props.pedido.items.map((item: any, index: number) => (
+                                    <li
+                                        className="list-group-item border-0 bg-transparent p-0 my-1"
+                                        key={index}
+                                    >
+                                        <span className="card-text p-1 px-2 rounded-2 bg-primary bg-opacity-25">
+                                            <small className="">
+                                                {item.descricao} R${" "}
+                                                {item.valor.toFixed(2).replace(".", ",")}
+                                            </small>
+                                        </span>
+                                    </li>
+                                ))}
+                            </ul>
+                        </Col>
+
+                        {props.isPrincipal ? (
+                            <Col>
+                                <Detalhes pedido={props.pedido}></Detalhes>
+                            </Col>
+                        ) : null}
+                    </Row>
                 </Col>
             </Row>
         </Card>
--- src/components/Detalhes/Detalhes.tsx
+++ src/components/Detalhes/Detalhes.tsx
@@ -1,12 +1,12 @@
 import { Card, Col, Row } from "react-bootstrap"
+import "./Detalhes.scss";
 
 export const Detalhes = ({ ...props }) => {
 
     return (
-        <Card className="h-100 border-0 p-2 bg-transparent">
-            <Row className={`h-100 rounded-end bg-primary bg-opacity-10`}>
+        <Card className="h-100 border-0 px-2 bg-transparent">
+            <Row className={`h-100 border-0 rounded-chat-start bg-primary bg-opacity-10`}>
                 <Col className="col-md-12 p-3">
-                    <p className="">Área para mais detalhes do pedido...</p>
                     <p className="fw-semibold">Obs: {props.pedido.obs}</p>
                 </Col>
             </Row>

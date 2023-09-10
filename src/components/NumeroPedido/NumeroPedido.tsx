029d78cae47513f10da5e116c59b65f3840f27b7
Joel Estumano <joelestumano@gmail.com>
2023-09-10 19:41:31 -0300

    fake protocol ajuste mês

--- src/components/NumeroPedido/NumeroPedido.tsx
+++ src/components/NumeroPedido/NumeroPedido.tsx
@@ -2,11 +2,11 @@ export function NumeroPedido({ ...props }) {
     return (
         <h6 className={`fw-semibold ${props.isPrincipal ? "fs-4" : ""}`}>
             Pedido
-            <span className="ms-2 bg-warning rounded-start ps-2">
+            <span className="ms-2 bg-warning rounded-start ps-2 pe-1">
                 {props.value.slice(0, 6)}
             </span>
-            <span className="bg-success text-white rounded-end pe-2">
-                {props.value.slice(6)}
+            <span className="bg-success text-white rounded-end ps-1 pe-2">
+                {(props.value.slice(6)).replace(' ', '')}
             </span>
         </h6>
     );

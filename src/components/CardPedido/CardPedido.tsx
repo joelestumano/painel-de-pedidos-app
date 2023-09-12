6f383400d972055b5b37c4f517de301c748d5ea7
Joel Estumano <joelestumano@gmail.com>
2023-09-12 14:38:46 -0300

    subtrai 10 min

--- src/components/CardPedido/CardPedido.tsx
+++ src/components/CardPedido/CardPedido.tsx
@@ -15,11 +15,34 @@ const TextTitle = ({ ...props }) => {
 };
 
 export const CardPedido = ({ ...props }) => {
+
     function getImgs(pedido: Pedido): string[] {
         return pedido.items.map((item: any, index: number) => item.bannerUrl);
     }
 
-    let despacho = new Date(props.pedido?.horaDespacho);
+
+    function subtractTenMinutes(dateTimeString: string): string {
+
+        const [date, time] = dateTimeString.split(" ");
+        const [year, month, day] = date.split("-");
+        const [hours, minutes, seconds] = time.split(":");
+
+        const dateTime = new Date(Number(year), Number(month) - 1, Number(day), Number(hours), Number(minutes), Number(seconds));
+
+        dateTime.setMinutes(dateTime.getMinutes() - 10);
+
+        return `${dateTime.getFullYear()}-${(dateTime.getMonth() + 1)
+            .toString()
+            .padStart(2, "0")}-${dateTime.getDate().toString().padStart(2, "0")} ${dateTime
+                .getHours()
+                .toString()
+                .padStart(2, "0")}:${dateTime.getMinutes().toString().padStart(2, "0")}:${dateTime
+                    .getSeconds()
+                    .toString()
+                    .padStart(2, "0")}.${dateTime.getMilliseconds()}`;
+    }
+
+    let despacho = new Date(subtractTenMinutes(props.pedido?.horaDespacho));
 
     return (
         <Card className="h-100 border-0 p-2 bg-transparent">

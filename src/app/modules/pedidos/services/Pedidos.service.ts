a6860450f574d2b870f358440a2d29da1ff7527d
Joel Estumano <joelestumano@gmail.com>
2023-09-12 23:35:05 -0300

    primeira etapa de estruturação (axios, rename)

--- src/app/modules/pedidos/services/Pedidos.service.ts
+++ src/app/modules/pedidos/services/Pedidos.service.ts
@@ -1,12 +1,12 @@
 import { Paginate } from "../../../shared/types/Paginate.type";
-import { Api } from "../../../services/Api.service";
+import { ApiService } from "../../../services/Api.service";
 
 const limit: number = 1000;
 const status: string = "pendente";
 const baseURL: string = "v1/pedidos/"
 
 const getPaginate = async (): Promise<Paginate> => {
-    const { data } = await Api().get(`${baseURL}paginate?ativarPaginacao=true&pagina=1&limite=${limit}&status=${status}`);
+    const { data } = await ApiService().get(`${baseURL}paginate?ativarPaginacao=true&pagina=1&limite=${limit}&status=${status}`);
     return data;
 };
 

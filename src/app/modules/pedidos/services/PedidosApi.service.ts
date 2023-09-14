1e93fdaa5689cefab368806dbc2939af768f557b
Joel Estumano <joelestumano@gmail.com>
2023-09-14 09:51:42 -0300

    time

--- src/app/modules/pedidos/services/PedidosApi.service.ts
+++ src/app/modules/pedidos/services/PedidosApi.service.ts
@@ -7,10 +7,14 @@ const baseURL: string = "v1/pedidos/"
 
 const start: Date = new Date();
 const end: Date = new Date();
+const timeDifferenceServer = 0;
 
-const dateStart: string = start.toISOString().slice(0, 10);
-end.setDate(start.getDate() + 2)
-const dateEnd: string = end.toISOString().slice(0, 10);
+start.setHours(start.getHours() + timeDifferenceServer);
+end.setHours(end.getHours() + timeDifferenceServer);
+end.setDate(end.getDate() + 1);
+
+const dateStart: string = start.toISOString().replace('T', ' ').replace('Z', '');
+const dateEnd: string = end.toISOString().replace('T', ' ').replace('Z', '');
 
 const getPaginate = async (): Promise<Paginate> => {
     const { data } = await ApiService().get(`${baseURL}paginate?ativarPaginacao=true&pagina=1&limite=${limit}&status=${status}&dateEnd=${dateEnd}&dateStart=${(dateStart)}`);

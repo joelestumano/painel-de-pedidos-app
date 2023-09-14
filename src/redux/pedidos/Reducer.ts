499ab7b59394336dd8163e96b3b8d57640fe82ce
Joel Estumano <joelestumano@gmail.com>
2023-09-14 11:23:37 -0300

    redux

--- src/redux/pedidos/Reducer.ts
+++ src/redux/pedidos/Reducer.ts
@@ -1,10 +1,17 @@
-const initialState = {
+import { PedidoType } from "../../app/modules/pedidos/types/Pedido.type";
+
+export enum ACTION_TYPE {
+    LISTAR_PEDIDOS = 'LISTAR_PEDIDOS'
+}
+
+const initialState: { pedidos: PedidoType[], pedidosAtrasados: number } = {
+    pedidos: [],
     pedidosAtrasados: 0
 }
 
-const PedidosReducer = (state = initialState, action: { type: string, payload: number }) => {
-    if (action.type === "PEDIDO_ATRASADO") {
-        return { ...state, pedidosAtrasados: state.pedidosAtrasados + action.payload };
+const PedidosReducer = (state = initialState, action: { type: string, payload: PedidoType[] }) => {
+    if (action.type === ACTION_TYPE.LISTAR_PEDIDOS) {
+        return { ...state, pedidos: action.payload };
     }
     return state;
 }

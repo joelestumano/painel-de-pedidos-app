3c632f7b9a1d29b69c87a53beba24e2361ed729f
Joel Estumano <joelestumano@gmail.com>
2023-09-15 11:48:27 -0300

    deploy is online fix

--- src/app/modules/pedidos/components/progressbar-pedido/progressbar-pedido.component.tsx
+++ src/app/modules/pedidos/components/progressbar-pedido/progressbar-pedido.component.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { ProgressBar } from "react-bootstrap";
-import { PedidoType } from "../../types/Pedido.type";
+import { PedidoType } from "../../types/pedido.type";
 import { PedidosTimeService } from "../../services/pedidos-time.service";
 
 export const ProgressbarPedidoComponent: React.FC<{ pedido: PedidoType }> = ({ pedido }) => {

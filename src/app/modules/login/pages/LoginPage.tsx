cf7bfc448c84f95fbda0ac6ca1df2f14adab067b
Joel Estumano <joelestumano@gmail.com>
2023-10-06 16:39:24 -0300

    email service

--- src/app/modules/login/pages/LoginPage.tsx
+++ src/app/modules/login/pages/LoginPage.tsx
@@ -133,7 +133,10 @@ export const LoginPage: React.FC<{}> = () => {
                                         placeholder={"Senha"}
                                     />
 
-                                    <a href="/forgotten-password" className="nav-link mb-4 text-md-end text-decoration-underline">esqueceu sua senha?</a>
+                                    <a href="/forgotten-password" className="nav-link mb-4 text-md-end text-decoration-underline">
+                                        <span className="me-1">esqueceu sua senha?</span>
+                                        <BsIconComponent iconName="PersonFillExclamation" />
+                                    </a>
 
                                     <SgButton
                                         type="submit"

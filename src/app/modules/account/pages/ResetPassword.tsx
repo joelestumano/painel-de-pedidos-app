181d8ea762d0376840e4dceb7af3bb96a7ba2a39
Joel Estumano <joelestumano@gmail.com>
2023-10-28 14:09:30 -0300

    styles

--- src/app/modules/account/pages/ResetPassword.tsx
+++ src/app/modules/account/pages/ResetPassword.tsx
@@ -148,7 +148,7 @@ export const ResetPasswordPage: React.FC<{}> = () => {
                                             />
                                             <span className="input-group-text border-primary p-0">
                                                 <SgButton type="button" variant="transparent" child={
-                                                    <BsIcon iconName={"ClipboardFill"} />
+                                                    <BsIcon iconName={"ClipboardFill"} title="colar token"/>
                                                 } />
                                             </span>
                                         </InputGroup>
